5201aad1b1ba1a203609e0691c8f9f9761b53942
Chris Feher <fehercb@gmail.com>
2015-07-27 10:21:12 -0400

    Added shared scheme

--- FearKit/FearKit/src/FearUI/View Controllers/FKMasterDetailViewController/FKMasterDetailViewController.swift
+++ FearKit/FearKit/src/FearUI/View Controllers/FKMasterDetailViewController/FKMasterDetailViewController.swift
@@ -19,7 +19,7 @@ class FKMasterDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-	}
+
 
 	override func viewDidAppear(animated: Bool) {
 		super.viewDidAppear(animated)
