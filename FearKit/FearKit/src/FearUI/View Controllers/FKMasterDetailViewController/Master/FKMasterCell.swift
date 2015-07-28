dd107534e2115fde0f37380147f22a7ea9a651bc
Chris Feher <fehercb@gmail.com>
2015-07-28 11:57:41 -0400

    Added debug code

--- FearKit/FearKit/src/FearUI/View Controllers/FKMasterDetailViewController/Master/FKMasterCell.swift
+++ FearKit/FearKit/src/FearUI/View Controllers/FKMasterDetailViewController/Master/FKMasterCell.swift
@@ -10,13 +10,59 @@ import UIKit
 
 public class FKMasterCell: UITableViewCell {
 
-	@IBOutlet weak var majorLabel: UILabel!
-	@IBOutlet weak var leftImageView: UIImageView!
+	public let majorLabel: UILabel = UILabel()
+	public var leftImage: UIImage?
+	private var leftImageView: UIImageView = UIImageView(frame: CGRectZero)
+	private let padding: CGFloat = 10
 
-    override public func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+	public override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+		self.addSubview(self.majorLabel)
+		self.addSubview(self.majorLabel)
+
+		//autolayout
+		self.addConstraint(NSLayoutConstraint(
+			item: self.majorLabel,
+			attribute: NSLayoutAttribute.Left,
+			relatedBy: NSLayoutRelation.Equal,
+			toItem: self.leftImageView,
+			attribute: NSLayoutAttribute.Right,
+			multiplier: 1.0,
+			constant: self.padding))
+
+		self.addConstraint(NSLayoutConstraint(
+			item: self.majorLabel,
+			attribute: NSLayoutAttribute.Right,
+			relatedBy: NSLayoutRelation.Equal,
+			toItem: self,
+			attribute: NSLayoutAttribute.Right,
+			multiplier: 1.0,
+			constant: -self.padding))
+
+		self.addConstraint(NSLayoutConstraint(
+			item: self.majorLabel,
+			attribute: NSLayoutAttribute.Top,
+			relatedBy: NSLayoutRelation.Equal,
+			toItem: self,
+			attribute: NSLayoutAttribute.Top,
+			multiplier: 1.0,
+			constant: 0))
+
+		self.addConstraint(NSLayoutConstraint(
+			item: self.majorLabel,
+			attribute: NSLayoutAttribute.Bottom,
+			relatedBy: NSLayoutRelation.Equal,
+			toItem: self,
+			attribute: NSLayoutAttribute.Bottom,
+			multiplier: 1.0,
+			constant: 0))
+
+	}
+
+	required public init(coder aDecoder: NSCoder) {
+	    fatalError("init(coder:) has not been implemented")
+	}
 
     override public func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
