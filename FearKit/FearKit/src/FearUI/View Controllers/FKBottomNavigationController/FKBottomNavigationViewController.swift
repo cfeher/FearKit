059ab07c9e8a7a484bb86d2e99124d20a06424b1
Chris Feher <fehercb@gmail.com>
2015-07-29 15:17:14 -0400

    Fixing upsidedown text

--- FearKit/FearKit/src/FearUI/View Controllers/FKBottomNavigationController/FKBottomNavigationViewController.swift
+++ FearKit/FearKit/src/FearUI/View Controllers/FKBottomNavigationController/FKBottomNavigationViewController.swift
@@ -10,13 +10,9 @@ public class FKBottomNavigationViewController: UINavigationController {
 			self.view.frame.size.height - self.navigationBar.frame.size.height,
 			self.navigationBar.frame.size.width,
 			self.navigationBar.frame.size.height)
-	}
-	override public init(rootViewController: UIViewController) {
-		super.init(rootViewController: rootViewController)
-		rootViewController.navigationItem.titleView?.transform = CGAffineTransformScale(rootViewController.navigationItem.titleView!.transform, 1, -1)
-	}
-	required public init(coder aDecoder: NSCoder) {
-	    fatalError("init(coder:) has not been implemented")
-	}
 
+		for vc in self.viewControllers {
+			vc.navigationItem.titleView?.transform = CGAffineTransformScale(vc.navigationItem.titleView!.transform, 1, -1)
+		}
+	}
 }
