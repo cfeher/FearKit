984f912de04d7ebc217e8887d072773bdd797173
Chris Feher <fehercb@gmail.com>
2015-08-04 11:50:27 -0400

    tableview height update

--- FearKit/FearKit/src/FearUI/Views/FKTableView/FKTableView.swift
+++ FearKit/FearKit/src/FearUI/Views/FKTableView/FKTableView.swift
@@ -136,7 +136,7 @@ public class FKTableView: UIView, UITableViewDelegate, UITableViewDataSource {
 	private func updateTableViewHeight() {
 		var totalHeight: CGFloat = 0
 		if self.numRows > 0 {
-			for row in 0...self.numRows {
+			for row in 0...self.numRows - 1 {
 				println(row)
 				totalHeight += self.tableView(self.tableView, heightForRowAtIndexPath: NSIndexPath(forRow: row, inSection: 0))
 			}
