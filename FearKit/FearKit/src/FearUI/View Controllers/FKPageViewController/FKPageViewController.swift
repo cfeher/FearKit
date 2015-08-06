83a13b5068b6dedbea37a79a9f77ca42ca674662
Chris Feher <fehercb@gmail.com>
2015-08-06 11:35:07 -0400

    paging

--- FearKit/FearKit/src/FearUI/View Controllers/FKPageViewController/FKPageViewController.swift
+++ FearKit/FearKit/src/FearUI/View Controllers/FKPageViewController/FKPageViewController.swift
@@ -75,78 +75,75 @@ public class FKPageViewController: UIViewController, UIPageViewControllerDataSou
 extension FKPageViewController {
 	public func addPageView(view: FKPageView) {
 
+		//setup new view controller
 		let viewController = UIViewController(nibName: nil, bundle: nil)
 		viewController.view.addSubview(view)
 		view.setTranslatesAutoresizingMaskIntoConstraints(false)
 
+		//Add Constraints
+		viewController.view.addConstraint(NSLayoutConstraint(
+			item: viewController.view,
+			attribute: .Width,
+			relatedBy: .Equal,
+			toItem: view,
+			attribute: .Width,
+			multiplier: 1.0,
+			constant: 0))
+		viewController.view.addConstraint(NSLayoutConstraint(
+			item: viewController.view,
+			attribute: .Height,
+			relatedBy: .Equal,
+			toItem: view,
+			attribute: .Height,
+			multiplier: 1.0,
+			constant: 0))
+		viewController.view.addConstraint(NSLayoutConstraint(
+			item: view,
+			attribute: .Left,
+			relatedBy: .Equal,
+			toItem: viewController.view,
+			attribute: .Left,
+			multiplier: 1.0,
+			constant: 0))
+		viewController.view.addConstraint(NSLayoutConstraint(
+			item: view,
+			attribute: .Top,
+			relatedBy: .Equal,
+			toItem: viewController.view,
+			attribute: .Top,
+			multiplier: 1.0,
+			constant: 0))
+		viewController.view.addConstraint(NSLayoutConstraint(
+			item: view,
+			attribute: .Right,
+			relatedBy: .Equal,
+			toItem: viewController.view,
+			attribute: .Right,
+			multiplier: 1.0,
+			constant: 0))
+		viewController.view.addConstraint(NSLayoutConstraint(
+			item: view,
+			attribute: .Bottom,
+			relatedBy: .Equal,
+			toItem: viewController.view,
+			attribute: .Bottom,
+			multiplier: 1.0,
+			constant: 0))
+
+		//setup pageviewcontroller
 		self.currentIndex = 0
-		self.pageViewController.view.removeFromSuperview()
-		self.pageViewController.removeFromParentViewController()
-		self.pageViewController = UIPageViewController(
-			transitionStyle: .Scroll,
-			navigationOrientation: .Horizontal,
-			options: nil)
 		self.pageViewController.dataSource = self
-		self.addChildViewController(self.pageViewController)
 		self.pages.append(FKPageViewContainer(controller: viewController, pageView: view))
 		self.pageViewController.setViewControllers(
 			[self.viewControllersFromPages()[0]],
 			direction: .Forward,
 			animated: true,
 			completion: nil)
+
+		//adjust page size - TODO: NOT NEEDED?
 		self.pageSize = CGSize(
 			width: view.frame.size.width,
 			height: view.frame.size.height)
-		self.view.addSubview(self.pageViewController.view)
-
-		viewController.view.addConstraint(NSLayoutConstraint(
-			item: viewController.view,
-			attribute: .Width,
-			relatedBy: .Equal,
-			toItem: view,
-			attribute: .Width,
-			multiplier: 1.0,
-			constant: 0))
-		viewController.view.addConstraint(NSLayoutConstraint(
-			item: viewController.view,
-			attribute: .Height,
-			relatedBy: .Equal,
-			toItem: view,
-			attribute: .Height,
-			multiplier: 1.0,
-			constant: 0))
-		viewController.view.addConstraint(NSLayoutConstraint(
-			item: view,
-			attribute: .Left,
-			relatedBy: .Equal,
-			toItem: viewController.view,
-			attribute: .Left,
-			multiplier: 1.0,
-			constant: 0))
-		viewController.view.addConstraint(NSLayoutConstraint(
-			item: view,
-			attribute: .Top,
-			relatedBy: .Equal,
-			toItem: viewController.view,
-			attribute: .Top,
-			multiplier: 1.0,
-			constant: 0))
-		viewController.view.addConstraint(NSLayoutConstraint(
-			item: view,
-			attribute: .Right,
-			relatedBy: .Equal,
-			toItem: viewController.view,
-			attribute: .Right,
-			multiplier: 1.0,
-			constant: 0))
-		viewController.view.addConstraint(NSLayoutConstraint(
-			item: view,
-			attribute: .Bottom,
-			relatedBy: .Equal,
-			toItem: viewController.view,
-			attribute: .Bottom,
-			multiplier: 1.0,
-			constant: 0))
 	}
 
 	private func viewControllersFromPages() -> [UIViewController] {
