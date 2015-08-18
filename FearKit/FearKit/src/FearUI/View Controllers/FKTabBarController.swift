8afad5fe27b7c84b38039366ad93136d099ae407
Chris Feher <fehercb@gmail.com>
2015-08-18 11:53:07 -0400

    Fixing public

--- FearKit/FearKit/src/FearUI/View Controllers/FKTabBarController.swift
+++ FearKit/FearKit/src/FearUI/View Controllers/FKTabBarController.swift
@@ -61,7 +61,7 @@ public class FKTabBarController: UIViewController {
 			constant: 0))
 	}
 
-	required init(coder aDecoder: NSCoder) {
+	required public init(coder aDecoder: NSCoder) {
 	    fatalError("init(coder:) has not been implemented")
 	}
 }
