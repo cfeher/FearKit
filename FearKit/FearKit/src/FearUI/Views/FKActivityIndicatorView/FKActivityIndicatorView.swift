7247dff8aa32e14793bf2445eb2018b692e1b592
Chris Feher <fehercb@gmail.com>
2015-10-09 10:31:05 -0400

    Added base activity indicator view

---
+++ FearKit/FearKit/src/FearUI/Views/FKActivityIndicatorView/FKActivityIndicatorView.swift
@@ -0,0 +1,21 @@
+//
+//  FKActivityIndicatorView.swift
+//  FearKit
+//
+//  Created by Chris Feher on 2015-10-09.
+//  Copyright (c) 2015 Chris Feher. All rights reserved.
+//
+
+import UIKit
+
+class FKActivityIndicatorView: UIActivityIndicatorView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
