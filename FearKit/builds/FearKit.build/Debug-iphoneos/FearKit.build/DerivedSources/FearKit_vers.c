30783879713b0a1b88d937a053f09126ccb5fa96
Chris Feher <fehercb@gmail.com>
2015-10-15 09:53:28 -0400

    Removing unnecessary files

--- FearKit/builds/FearKit.build/Debug-iphoneos/FearKit.build/DerivedSources/FearKit_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char FearKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FearKit  PROJECT:FearKit-1" "\n"; const double FearKitVersionNumber __attribute__ ((used)) = (double)1.;
--- FearKit/builds/FearKit.build/Debug-iphonesimulator/FearKit.build/Objects-normal/x86_64/FearKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/cfeher/Dropbox/Documents/FearKit/FearKit/builds/FearKit.build/Debug-iphonesimulator/FearKit.build/DerivedSources/FearKit_vers.c
